c44bc3eaa74d159ebef9c3a82bb825720bdae691
Sheng Cheng <charliecheng19882006@gmail.com>
2021-11-12 17:59:25 -0500

    code

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const SAT_API_KEY = "6Y6N2Y-EK63AU-VTKBD6-4SI9";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
